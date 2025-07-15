a936d7ec4e0f3d962a0e454aaf5e9a91b3eb791b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-07-15 18:20:02 +0700

    feat(client): add renderPromise

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -1,70 +1,25 @@
-const untilPredicates: Map<
-    number,
-    {
-        predicate: () => boolean;
-        resolve: () => void;
-        accMs: number;
-        intervalMs: number;
-    }
-> = new Map();
+export const renderPromise = <T>(
+    fn: (
+        resolve: (value: T | PromiseLike<T>) => void,
+        reject: (reason?: any) => void
+    ) => void
+) => {
+    let scopedResolve: (value: T | PromiseLike<T>) => void = undefined!;
+    let scopedReject: (reason?: any) => void = undefined!;
+    const promise = new Promise<T>((resolve, reject) => {
+        scopedResolve = (value) => {
+            resolve(value);
+            mp.events.remove('render', wrapped);
+        };
+        scopedReject = (reason) => {
+            reject(reason);
+            mp.events.remove('render', wrapped);
+        };
+    });
 
-let untilCounter = 0;
-let untilRenderAdded = false;
-
-/**
- * Returns a function that waits until the given predicate returns true, optionally with a timeout.
- * The returned function registers the predicate and resolves a promise when the predicate is satisfied.
- * This waiting is implemented using the game's 'render' event, not by polling or using setInterval.
- * If a timeout is specified and reached before the predicate returns true, the promise is rejected.
- *
- * @param predicate - A function that returns a boolean indicating whether the condition is met.
- * @returns A function that takes an optional timeout (in milliseconds) and waits for the predicate to be satisfied.
- */
-export const until = (predicate: () => boolean) => {
-    return (options?: { timeoutMs?: number; intervalMs?: number }) => {
-        const id = ++untilCounter;
-        let scopedResolve: () => void = undefined!;
-        let timeout: NodeJS.Timeout | undefined;
-        const promise = new Promise<void>((resolve, reject) => {
-            scopedResolve = resolve;
-            if (options?.timeoutMs != null) {
-                timeout = setTimeout(() => {
-                    reject(
-                        'Timeout reached while waiting for predicate to be true'
-                    );
-                }, options.timeoutMs);
-            }
-        }).finally(() => {
-            if (timeout != null) {
-                clearTimeout(timeout);
-            }
-            untilPredicates.delete(id);
-        });
-        untilPredicates.set(id, {
-            predicate,
-            resolve: scopedResolve,
-            accMs: 0,
-            intervalMs: options?.intervalMs ?? 0,
-        });
-        if (!untilRenderAdded) {
-            mp.events.add('render', handleUntilPredicatesOnRender);
-            untilRenderAdded = true;
-        }
-        return promise;
+    const wrapped = () => {
+        fn(scopedResolve, scopedReject);
     };
-};
-
-const handleUntilPredicatesOnRender = () => {
-    for (const [_, a] of untilPredicates) {
-        if (a.intervalMs > 0) {
-            a.accMs += mp.game.gameplay.getFrameTime();
-            if (a.accMs < a.intervalMs) {
-                continue;
-            }
-            a.accMs = 0;
-        }
-        if (a.predicate()) {
-            a.resolve();
-        }
-    }
+    mp.events.add('render', wrapped);
+    return promise;
 };
